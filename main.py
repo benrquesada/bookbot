9e8a66438823b18cb70f2945aa3d9aef40cb97f6
benrquesada <benrquesada@gmail.com>
2025-04-16 16:01:40 -0700

    finish basic project

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import word_count
+from stats import word_count, character_count, sorted_caracter_dicts
+import sys
 
 def get_book_text(filepath):
     with open(filepath, 'r', encoding='utf-8') as file:
@@ -7,6 +8,33 @@ def get_book_text(filepath):
     
 
 def main ():
-    print(f"{word_count(get_book_text("./books/frankenstein.txt"))} words found in the document")
 
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+        
+
+
+    filepath = sys.argv[1]
+    book_text = get_book_text(filepath)
+    wc = word_count(book_text)
+    character_dict = character_count(book_text)
+    sorted_caracter_dict_list = sorted_caracter_dicts(character_dict)
+    
+    
+    
+    
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {wc} total words")
+    print("--------- Character Count -------")
+    for dict in sorted_caracter_dict_list:
+        letter = dict["character"]
+        if letter.isalpha():
+            print(f"{dict["character"]}: {dict["count"]}")
+    print("============= END ===============")
+    
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,27 @@
 def word_count(text):
     words = text.split()
     return len(words)
+
+
+def character_count(text):
+    character_dict = {}
+    lower_text = text.lower()
+    words = lower_text.split()
+    for word in words:
+        for character in word:
+            if character in character_dict:
+                character_dict[character] = character_dict[character] + 1
+            else:
+                character_dict[character] = 1 
+    return character_dict
+
+
+def sorted_caracter_dicts(character_dict):
+    def sort_on(dict):
+        return dict["count"]
+    dict_list = []
+    for key,value in character_dict.items():
+        dict_list.append({"character": key, "count": value})
+    dict_list.sort(reverse=True, key=sort_on)
+            
+    return dict_list
